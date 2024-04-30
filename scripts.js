b4e5560405cb868adb780aea8817361176255bfe
Kevin Lopes <167485656+KevLps@users.noreply.github.com>
2024-04-30 08:59:19 -0300

    scripts.js
    JavaScript code

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".formulario")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotao(){
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+
+}
+function cliqueiFora(){
+    form.style.left = "-322px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+}
+    
